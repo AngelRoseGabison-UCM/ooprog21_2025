62660ce5755bfa6400df7cab1638f45c5ff42332
AngelRoseGabison-UCM <argabison11@gmail.com>
2025-09-18 01:15:12 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single-line comment
+        System.out.println("Hello, this line is after a single-line comment!");
+
+        /*
+          This is a multi-line comment.
+          It can span across multiple lines.
+         */
+        System.out.println("This line comes after a multi-line comment.");
+
+        /** 
+          This is a documentation comment.
+          It is usually used for generating external documentation using Javadoc.
+         */
+        System.out.println("This line follows a documentation comment.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("The house that you live in don't make it a home, But feeling lonely don't mean you're alone.");
+        System.out.println("All of my friends are missing again. That's what happens when you fall in love.");
+        System.out.println("Me and my truth, we sit in silence. Mm, baby girl, it's just me and you 'Cause I don't wanna argue.");
+        System.out.println("I used to float, now I just fall down, I used to know but I'm not sure now. What I was made for. What was I made for?");
+    }
+}
